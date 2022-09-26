f02415c8fd58041df80ee90ef5ec0d9ba03d929b
Replit user <>
2022-09-26 01:34:20 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -47,9 +47,9 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
   TreeNode * newNode = (TreeNode *)malloc(sizeof(TreeNode));
-  TreeNode * aux = (TreeNode *)malloc(sizeof(TreeNode));
+  TreeNode * aux;
   aux->pair = searchTreeMap(tree,key);
-  if (aux->pair==NULL){
+  if (aux->pair == NULL){
     newNode=createTreeNode(key,value);
     tree->current=newNode;
   }
