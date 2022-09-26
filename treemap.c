cfd18d2f496fadb1d982a0beb3d5bef9254da111
Replit user <>
2022-09-26 03:35:25 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -169,13 +169,6 @@ Pair * upperBound(TreeMap * tree, void* key) {
     ub_node=aux->parent;
     tree->current=aux;
   }
-  while(ub_node!=NULL){
-    if(tree->lower_than(ub_node->pair->key,key)==1){
-      return ub_node->pair;
-    }
-    ub_node=ub_node->parent;
-  }
-  return NULL;
 }
 
 Pair * firstTreeMap(TreeMap * tree) {
